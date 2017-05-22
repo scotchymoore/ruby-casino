fbbbb8ea7fc2af1f7819b1c51f7f3bbfee5f0b24
Stockton Hedges <stockton@Stocktons-MacBook-Air.local>
2017-05-22 11:19:36 -0600

    initial menu

--- casino.rb
+++ casino.rb
@@ -17,7 +17,24 @@ class Casino
     #Game options
     #Bankroll
     #Quit
+    puts """Game Menu:\n1)Heads or Tails\n2)Blackjack\n3)Slots\n4)Roulette
+  5)21\n6)Go to Casino locations\n7)Exit"""
+  input = gets.to_i
+  case input
+  when 1
     HeadsTails.new(@player)
+  when 2
+    blackjack.new(@player)
+  when 3
+    slots.new(@player)
+  when 4
+    roulette.new(@player)
+  when 5
+    twenty_one.new(@player)
+  when 6
+    locations_menu
+  when 7
+    exit
   end
 end
 
