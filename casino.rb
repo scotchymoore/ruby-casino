674a84bf4570d2010793a4bddc32f0ddd208629b
Stockton Hedges <stockton@Stocktons-MacBook-Air.local>
2017-05-22 10:51:38 -0600

    headstail

--- casino.rb
+++ casino.rb
@@ -2,15 +2,22 @@ require 'pry'
 require 'colorize'
 
 require_relative 'player'
-
-
+require_relative 'heads-tails'
 
 class Casino
   attr_accessor :player
   def initialize
     puts "Welcome to our Ruby Casino!"
     @player = Player.new
-    binding.pry
+    casino_menu
+  end
+
+  def casino_menu
+    #Casino options
+    #Game options
+    #Bankroll
+    #Quit
+    HeadsTails.new(@player)
   end
 end
 
---
+++ heads-tails.rb
@@ -0,0 +1,6 @@
+class HeadsTails
+  def initialize(player)
+    puts "Welcome to Heads Tails!"
+    puts "#{player.name} you have a balance of #{player.wallet.amount}."
+  end
+end
