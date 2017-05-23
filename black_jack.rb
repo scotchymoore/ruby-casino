2cab88db8a9b0217a5fa2aca6a531fd9352acdd5
Scott Moore <scotchymoore@gmail.com>
2017-05-23 12:09:52 -0600

    black jack

--- black_jack.rb
+++ black_jack.rb
@@ -17,11 +17,12 @@ class BlackJack
 
   def place_bet
     puts "How much do you want to bet? "
+    @bet = 0
     @bet = gets.to_i
-    if bet > @player.wallet.amount
+    if @bet > @player.wallet.amount
       puts "Sorry sir. You can't bet what you don't have. Try a lower amount."
       place_bet
-    else bet <= @player.wallet.amount
+    else @bet <= @player.wallet.amount
       puts "All bets are down. They can't be changed."
       deal
     end
@@ -30,10 +31,10 @@ class BlackJack
   def deal
     puts "Let me shuffle the cards."
     puts "Dealer is now dealing.\n\n"
-    dealer_cards = @deck.sample(2)
-    dc = dealer_cards[0]
+    @dealer_cards = @deck.sample(2)
+    dc = @dealer_cards[0]
       puts "Dealer has #{dc.rank} of #{dc.suit}"
-    @dealer_value = dealer_cards.inject(0) { |total, card| total += card.value.to_i }
+    @dealer_value = @dealer_cards.inject(0) { |total, card| total += card.value.to_i }
     player_cards = @deck.sample(2)
     @player_value = 0
     @player_value = player_cards.inject(0) { |total, card| total += card.value.to_i }
@@ -46,11 +47,12 @@ class BlackJack
     puts "#{player.name} has #{@player_value}\n\n"
 
     if @player_value == 21
-      "\n\nBlackjack!  Winner Winner Chicken Dinner!\n"
+      puts "\n\nBlackjack!  Winner Winner Chicken Dinner!\n"
       winning_amount = bet* 2
       @player.wallet.amount = @player.wallet.amount + winning_amount
       puts "#{player.name}, you just won $#{winning_amount}."
       puts "You now have $#{@player.wallet.amount}.\n"
+      play_again
     else
     hit_or_stay
   end
@@ -82,26 +84,60 @@ def hit
   if nv < 21
     hit_or_stay
   elsif nv == 21
-    "\n\nBlackjack!  Winner Winner Chicken Dinner!\n"
-    winning_amount = @bet* 2
+    winning_amount = @bet* 3
     @player.wallet.amount = @player.wallet.amount + winning_amount
+    puts "\n\nBlackjack!  Winner Winner Chicken Dinner!\n"
     puts "#{player.name}, you just won $#{winning_amount}."
     puts "You now have $#{@player.wallet.amount}.\n"
+    play_again
   else
     puts "You LOSE!  House wins!"
-    @player.wallet.amount = @player.wallet.amount - @bet
+    @player.wallet.amount = @player.wallet.amount -= @bet
     puts " You lost $#{@bet}.  You now have $#{@player.wallet.amount} left."
-    main_black_jack
+    play_again
   end
 end
 
 def end_of_game
-
-
+  puts "The dealer has"
+  @dealer_cards.each do |pc|
+    puts "Dealer have #{pc.rank} of #{pc.suit}"
+  end
+    puts "The Dealer has #{@dealer_value}"
+    if @dealer_value < 17
+      dealer_hit
+      puts
+      puts
+      if @dealer_value == 21
+        puts "You LOSE!  House wins!"
+        @player.wallet.amount = @player.wallet.amount -= @bet
+        puts " You lost $#{@bet}.  You now have $#{@player.wallet.amount} left."
+        play_again
+      elsif @dealer_value > 21
+        puts "You won!"
+        @player.wallet.amount = @player.wallet.amount += (@bet * 1.5)
+        puts " You won $#{@bet}.  You now have $#{@player.wallet.amount} left."
+        play_again
+      elsif @dealer_value > @nv
+        puts "You LOSE!  House wins!"
+        @player.wallet.amount = @player.wallet.amount -= @bet
+        puts " You lost $#{@bet}.  You now have $#{@player.wallet.amount} left."
+        play_again
+      elsif @nv > @dealer_value
+        puts  "You won!"
+        @player.wallet.amount = @player.wallet.amount += (@bet * 1.5)
+        puts " You won $#{@bet}.  You now have $#{@player.wallet.amount} left."
+        play_again
+      else @nv == @dealer_value
+        puts "Its a push.  No winner or loser."
+        puts "Lets play again."
+        play_again
+    end
+  end
 end
 
   def main_black_jack
-    puts "Do you want to play?
+    puts "\nDo you want to play?
     1. Yes
     2. No"
     play = gets.to_i
@@ -118,7 +154,7 @@ end
   end
 
   def play_again
-    puts "Do you want to play again?
+    puts "\n\nDo you want to play again?
     1. Yes
     2. No"
     play = gets.to_i
@@ -127,11 +163,21 @@ end
       puts "Well alright then, grab a seat and lets play."
       place_bet
     when 2
-      puts "Thanks for stopping by, it was fun while it lasted."
-      casino_menu
+      puts "Thanks for stopping by, it was fun while it lasted.\n\n"
     else
       puts "I said are you going to play or leave.  Make up your mind!"
       play_again
+    end
+  end
+
+  def dealer_hit
+  new_card = @deck.sample
+  ncv = new_card.value.to_i
+  @dealer_value += ncv
+  puts "The Dealer got a #{new_card.rank} of #{new_card.suit}"
+  puts "The Dealer has #{@dealer_value}. "
+    if @dealer_value < 17
+      dealer_hit
     end
   end
 end
