0ad338981c9e1065659216d60566db20d34ffb91
Scott Moore <scotchymoore@gmail.com>
2017-05-23 10:01:42 -0600

    black jack

--- black_jack.rb
+++ black_jack.rb
@@ -1,11 +1,13 @@
 require_relative 'deck'
 
+
 class BlackJack
   attr_accessor :deck, :player, :bet
 
   def initialize(player)
     @player = player
     @deck = Deck.new.shuffle_cards
+    puts "Welcome to Black Jack!\n"
 
     # get all cards from deck - @deck.cards
 
@@ -31,7 +33,7 @@ class BlackJack
     dealer_cards = @deck.sample(2)
     dc = dealer_cards[0]
       puts "Dealer has #{dc.rank} of #{dc.suit}"
-    dealer_value = dealer_cards.inject(0) { |total, card| total += card.value.to_i }
+    @dealer_value = dealer_cards.inject(0) { |total, card| total += card.value.to_i }
     player_cards = @deck.sample(2)
     @player_value = 0
     @player_value = player_cards.inject(0) { |total, card| total += card.value.to_i }
@@ -109,7 +111,6 @@ end
       place_bet
     when 2
       puts "Thanks for stopping by but I only talk to players. The all you can eat buffet is over there."
-      casino_menu
     else
       puts "I said are you going to play or leave.  Make up your mind!"
       main_black_jack
