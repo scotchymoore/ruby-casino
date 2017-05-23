991cfdad89750c719a23296c4db2a0ebe420b6cf
Stockton Hedges <stockton@stocktons-air.uconnect.utah.edu>
2017-05-23 15:26:54 -0600

    highlow finished

--- high_low.rb
+++ high_low.rb
@@ -1,6 +1,7 @@
 require 'pry'
 
 require_relative 'deck'
+# require_relative 'casino'
 
 class HighLow
   attr_accessor :bet_amount, :player_count, :bet_won
@@ -8,9 +9,9 @@ class HighLow
   def initialize (player)
     @player = player
     @player_count = 0
-    puts "Welcome to High or Low #{player.name}!"
-    puts "You have a balance of #{player.wallet.amount}."
-    puts "1)To play game\n2)To read rules\n3)Exit to menu"
+    puts "Welcome to High or Low #{player.name}!".green
+    puts "You have a balance of #{player.wallet.amount}.".green
+    puts "1)To play game\n2)To read rules\n3)Exit to menu".yellow
     choice = gets.to_i
     case choice
     when 1
@@ -20,7 +21,7 @@ class HighLow
     when 3
 
     else
-      puts "Invalid input, please try again."
+      puts "Invalid input, please try again.".green
       HighLow.new(@player)
     end
   end
@@ -30,79 +31,86 @@ class HighLow
 given the choice to pick higher lower. If the next number is the same
 as the choice you given of higher or lower, then you will be shown another
 number to do the same process. You must guess correctly 4 times in a row
-to win."""
-    puts "Please press 1 to play game."
+to win.""".green
+    puts "Please press 1 to play game.".yellow
     choice = gets.to_i
     if choice == 1
       bet
     else
-      puts "Invalid, please try again."
+      puts "Invalid, please try again.".green
       rules
     end
   end
 
   def bet
-    puts "How much would you like to bet?"
+    puts "How much would you like to bet?".green
     @bet_amount = gets.to_f
     if @bet_amount > @player.wallet.amount
-      puts "I am sorry, you do not have that much money. Please try again"
+      puts "I am sorry, you do not have that much money. Please try again".green
       bet
     end
-    puts "You have placed a bet of $#{bet_amount}. Is this correct?\n1)Yes\n2)No"
+    puts "You have placed a bet of $#{bet_amount}.".green
+    puts "Is this correct?\n1)Yes\n2)No".yellow
     input = gets.to_i
     if input == 1
       game_method
     elsif input == 2
-      puts "Please try again."
+      puts "Please try again.".green
       bet
     else
-      puts "I am sorry that is invalid input. Please try again."
+      puts "I am sorry that is invalid input. Please try again.".green
       bet
     end
   end
 
   def game_method
-    bet_won = @bet_amount * 2
+    @bet_won = @bet_amount * 2
     numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     a = numbers.sample
     b = numbers.sample
-    while @player_count < 4
-    puts "The high number is #{numbers.last} and the low is #{numbers.first}\nThe first number is..."
-    sleep (2)
-    puts "#{a}\n1)Higher\n2)Lower"
-    input = gets.to_i
-    if a < b
-      if input == 1
-        puts "Congrats! Your number is #{b}."
-        @player_count += 1
-        game_method
-      else
-        puts "Sorry! Your number is #{b}."
-        HighLow.new(@player)
+    while @player_count < 1
+      puts "The high number is #{numbers.last} and the low is #{numbers.first}\nThe first number is...".green
+      sleep (2)
+      puts "#{a}\n1)Higher\n2)Lower".yellow
+      input = gets.to_i
+      if a < b
+        if input == 1
+          puts "Congrats! Your number is #{b}.".green
+          @player_count += 1
+          game_method
+        else
+          puts "Sorry! Your number is #{b}.".green
+          puts "You have lost #{bet_amount}".green
+          @player.wallet.amount -= @bet_amount
+          HighLow.new(@player)
+        end
       end
-    end
-    if a > b
-      if input == 2
-        puts "Congrats! Your number is #{b}."
-        @player_count += 1
-        game_method
-      else
-        puts "Sorry! Your number is #{b}."
-        puts "You have lost #{bet_amount}."
-        @player.wallet.amount -= @bet_amount
-        HighLow.new(@player)
+      if a > b
+        if input == 2
+          puts "Congrats! Your number is #{b}.".green
+          @player_count += 1
+          game_method
+        else
+          puts "Sorry! Your number is #{b}.".green
+          puts "You have lost #{bet_amount}".green
+          @player.wallet.amount -= @bet_amount
+          HighLow.new(@player)
+        end
       end
-    end
-    end
-    puts "Congratulations! You have won #{bet_won}."
-    @player.wallet.amount += bet_won
-    puts "1)Play again\n2)Exit to the menu"
-    user_input = gets.to_i
-    case user_input
-    when 1
-      HighLow.new(@player)
-    else
-
+      puts "Congratulations! You have won #{@bet_won}.".green
+      @player.wallet.amount += @bet_won
+      puts "1)Play again\n2)Exit to the menu".yellow
+      user_input = gets.to_i
+      case user_input
+      when 1
+        HighLow.new(@player)
+      when 2
+        @player_count = 4
+        puts 'Thanks!'
+      end
     end
   end
+
+
 end
+puts 'end'
--- player.rb
+++ player.rb
@@ -7,20 +7,20 @@ class Player
     #need to think about how to implement the wallet
     # this could(should) be a class
     # or just an attribute on the user
-    puts 'What is your name?'
+    puts 'What is your name?'.red
     @name = gets.strip
-    puts "What is your age, #{@name}?"
+    puts "What is your age, #{@name}?".red
     @age = gets.strip.to_i
     if @age < 21
-      puts "I am sorry #{@name}, but you need to be at least 21 to come in here. Get older."
+      puts "I am sorry #{@name}, but you need to be at least 21 to come in here. Get older.".red
       exit
     end
     # puts "What is your gender, #{@name}?"
     # @gender = gets.strip
-    puts 'How much money are you playing with today?'
+    puts 'How much money are you playing with today?'.red
     amount = gets.to_f
     if amount < 1
-      puts "Sorry that is invalid, please try again."
+      puts "Sorry that is invalid, please try again.".red
       Player.new
     end
     @wallet = Wallet.new(amount)
